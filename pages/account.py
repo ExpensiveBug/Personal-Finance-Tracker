8156e9834596837223c810d37591c588d1f07f55
ExpensiveBug <nidhireddu40@gmail.com>
2026-02-02 08:12:51 +0530

    user data after login and logout

--- pages/account.py
+++ pages/account.py
@@ -1,35 +1,8 @@
 import streamlit as st
-import database as db
 import bcrypt
+import database as db
 
-def users_table():
-    conn = db.get_connection()
-    cursor = conn.cursor()
-    cursor.execute("""CREATE TABLE IF NOT EXISTS users_record(
-                   id INTEGER PRIMARY KEY AUTOINCREMENT,
-                   username TEXT UNIQUE NOT NULL,
-                   email TEXT UNIQUE NOT NULL,
-                   password TEXT NOT NULL
-                    )""")
-    conn.commit()
-    conn.close()
-
-def create_user(username, password, mail):
-    conn = db.get_connection()
-    cursor = conn.cursor()
-    # check if user already exist
-    cursor.execute("Select 1 FROM users_record WHERE username = ? and email = ?",
-                   (username, mail))
-    if cursor.fetchone():
-        conn.close()
-        return False
-    
-    hashed_pw = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
-    cursor.execute("INSERT INTO users_record (username, password, email) VALUES (?, ?, ?)",(username, hashed_pw, mail))
-    conn.commit()
-    conn.close()
-    return True
-
+# Old User account
 def user_verify(user, password):
     conn = db.get_connection()
     cursor = conn.cursor()
@@ -42,6 +15,7 @@ def user_verify(user, password):
         db_id, db_user, db_password, db_mail = result
         if bcrypt.checkpw(password.encode(), db_password):
             return {
+                "user_id" : db_id,
                 "username" : db_user,
                 "email" : db_mail
             }
@@ -55,12 +29,24 @@ def user_verify(user, password):
 # main function
 def show():
     st.header("Create Your :green[Account]")
-    users_table()
+    db.create_user_table()
 
     if "logged_in" not in st.session_state:
         st.session_state.logged_in = False
         st.session_state.username = None
         st.session_state.mail = None
+        st.session_state.user_id = None
+
+    # Show logout button if already logged in
+    if st.session_state.logged_in:
+        st.success(f"Welcome, {st.session_state.username}")
+        st.write(f"Email: {st.session_state.mail}")
+        if st.button("Logout", type="primary"):
+            db.delete_user_data(st.session_state.user_id)
+            st.session_state.clear()
+            st.success("Logged out successfully")
+            st.rerun()
+        return 
 
     acnt = st.pills("Account",["Login", "Sign Up"],selection_mode="single")
 
@@ -76,7 +62,8 @@ def show():
                     st.session_state.logged_in = True
                     st.session_state.username = user_data["username"]
                     st.session_state.mail = user_data["email"]
-                    st.success(f"Welcome, {st.session_state.username} 👋")
+                    st.session_state.user_id = user_data["user_id"]
+                    st.success(f"Welcome, {st.session_state.username}")
                     st.write(f"Email : {st.session_state.mail}")
                     st.rerun()
                 else : 
@@ -84,12 +71,11 @@ def show():
         else:  
             st.success(f"Welcome, {st.session_state.username}")
             st.write(f"Email : {st.session_state.mail}")
-            st.info("Are you sure you want to logout?")
+            st.warning("Are you sure you want to logout?")
 
             if st.button("Yes, Logout"):
-                st.session_state.logged_in = False
-                st.session_state.username = None
-                st.session_state.mail = None
+                db.delete_user_data(st.session_state.user_id)
+                st.session_state.clear()
                 st.success("Logged out successfully")
                 st.rerun()
 
@@ -101,16 +87,19 @@ def show():
         if st.button("Create my account", width = "stretch"):
             if not user or not password or not mail :
                 st.warning("Fill all the details !!")
-            elif create_user(user, password, mail) : 
-                st.snow()
-                st.session_state.logged_in = True
-                st.session_state.username = user
-                st.session_state.mail = mail
-                st.success("Yeah, Account created Succesfully :)")
-                st.success(f"Welcome, {user}")
-                st.write(f"Email : {mail}")
+            else:
+                user_id = db.create_user_account(user, password, mail)
+                if user_id : 
+                    st.snow()
+                    st.session_state.logged_in = True
+                    st.session_state.username = user
+                    st.session_state.mail = mail
+                    st.session_state.user_id = user_id
+                    st.success("Yeah, Account created Succesfully :)")
+                    st.success(f"Welcome, {user}")
+                    st.write(f"Email : {mail}")
+                    st.rerun()
 
-            else : 
-                st.error("Username or email already exist !!")
-
+                else : 
+                    st.error("Username or email already exist !!")
 
